31bc826549ddbd2ed505ac4bb2ae88367f0c039f
LTBS46 <40568322+LTBS46@users.noreply.github.com>
2018-09-03 19:21:12 +0200

    1.0.1.0

---
+++ Base203/scene/game/MAIN/SAVE_CALL.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+var save_path = "res://save/savegame.save"
+
+func _ready():
+	pass
+
+func _process(delta):
+	if Input.is_action_pressed("SAVE"):
+		save_game()
+
+func save_game():
+	var save_game = File.new()
+	save_game.open(save_path, File.WRITE)
+	var save_nodes = get_tree().get_nodes_in_group("Persist")
+	for i in save_nodes:
+		var node_data = i.call("save");
+		save_game.store_line(to_json(node_data))
+	save_game.close()
+
+func load_game():
+	var save_game = File.new()
+	if not save_game.file_exists(save_path):
+		return
+	var save_node = get_tree().get_nodes_in_group("Persist")
+	for i in save_node:
+		i.queue_free()
+	save_game.open(save_path, File.READ)
+	while not save_game.eof_reached():
+		var current_line = parse_json(save_game.get_line())
+		var new_object = load(current_line["filename"]).instance()
+		get_node(current_line["parent"]).add_child(new_object)
+		new_object.position = Vector2(current_line["pos_x"], current_line["pos_y"])
+		for i in current_line.keys():
+			if i == "filename" or i == "parent" or i == "pos_x" or i == "pos_y":
+				continue
+			new_object.set(i, current_line[i])
+	save_game.close()
--- Base203/scene/game/peons/CHIEF.gd
+++ Base203/scene/game/peons/CHIEF.gd
@@ -1,11 +1,22 @@
 extends Node2D
 
 var draged = false
-var name = "Chief"
-var 
+var zone = 0
+var z_pos = 0
 
 func _ready():
 	pass
 
 func _process(delta):
 	pass
+
+func save():
+	var save_dict = {
+		"filename" : get_filename(),
+		"parent" : get_parent().get_path(),
+		"pos_x" : position.x,
+		"pos_y" : position.y,
+		"zone" : zone,
+		"z_pos" : z_pos
+	}
+	return save_dict
