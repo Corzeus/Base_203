e5564c424b6eb8566e7761ca739bb5a61da22001
LTBS46 <40568322+LTBS46@users.noreply.github.com>
2018-09-06 21:15:26 +0200

    1.0.1.0.5

--- Base203/scene/Menu/OPTION/option.gd
+++ Base203/scene/Menu/OPTION/option.gd
@@ -1,10 +1,13 @@
 extends Node2D
 
 func _ready():
-	pass
+	get_tree().call_group("Auto_focus","grab_focus")
 
 func _process(delta):
-	pass
+	if Input.is_joy_known(0):
+		ProjectSettings.set_setting("Config/input/Controler_Mode",true)
+	else:
+		ProjectSettings.set_setting("Config/input/Controler_Mode",false)
 
 func _on_FULL_SCREEN_pressed():
 	if (OS.is_window_fullscreen()):
@@ -14,9 +17,6 @@ func _on_FULL_SCREEN_pressed():
 
 func _on_To_EN_pressed():
 	TranslationServer.set_locale("en")
-
-func _on_To_ES_pressed():
-	TranslationServer.set_locale("es")
 
 func _on_To_FR_pressed():
 	TranslationServer.set_locale("fr")
