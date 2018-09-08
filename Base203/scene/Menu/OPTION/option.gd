f3a4a2c954354286d284aa5bcdfac9b19eb2034e
LTBS46 <40568322+LTBS46@users.noreply.github.com>
2018-09-08 14:39:35 +0200

    1.0.1.1.1

--- Base203/scene/Menu/OPTION/option.gd
+++ Base203/scene/Menu/OPTION/option.gd
@@ -1,9 +1,21 @@
 extends Node2D
 
-func _ready():
-	get_tree().call_group("Auto_focus","grab_focus")
+const INPUT_ACTIONS = [ "UP", "DOWN", "LEFT", "RIGHT", "PAUSE", "ACCEPT", "SUB_SELECT" ]
+const CONFIG_FILE = "res://Resource/SPE/input.cfg"
 
-func _process(delta):
+var action
+var button
+
+func freeze_til_input():
+	get_tree().call_group("L1","Set(PAUSE_MODE_STOP)")
+
+func unfreeze_til_input():
+	get_tree().call_group("L1","Set(PAUSE_MODE_INHERIT)")
+
+func load_config():
+	pass
+
+func joy_mode():
 	if Input.is_joy_known(0):
 		ProjectSettings.set_setting("Config/input/Controler_Mode",true)
 		get_tree().call_group("Auto_focus","grab_focus")
@@ -14,6 +26,18 @@ func _process(delta):
 		get_tree().call_group("Auto_focus","grab_focus")
 		get_tree().call_group("KEYBOARD","Set(visible,true)")
 		get_tree().call_group("GAMEPAD","Set(visible,false)")
+
+func _ready():
+	if not ProjectSettings.get_setting("Config/input/OK"):
+		ProjectSettings.set_setting("Config/input/OK",true)
+		load_config()
+		get_tree().change_scene("res://scene/Menu/MAIN_MENU/MAIN.tscn")
+	get_tree().call_group("Auto_focus","grab_focus")
+	get_tree().call_group("KEYBOARD","Set(visible,true)")
+	get_tree().call_group("GAMEPAD","Set(visible,false)")
+
+func _process(delta):
+	joy_mode()
 
 func _on_FULL_SCREEN_pressed():
 	if (OS.is_window_fullscreen()):
@@ -26,3 +50,33 @@ func _on_To_EN_pressed():
 
 func _on_To_FR_pressed():
 	TranslationServer.set_locale("fr")
+
+func _on_UP_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
+
+func _on_DOWN_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
+
+func _on_LEFT_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
+
+func _on_RIGHT_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
+
+func _on_PAUSE_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
+
+func _on_ACCEPT_pressed():
+	freeze_til_input()
+	while not InputEventKey.pressed():
+		pass
